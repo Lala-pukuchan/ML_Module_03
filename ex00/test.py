09a872e60f3f94bf823ea6e1732cdd75959634a3
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-26 08:34:08 +0100

    ex00

--- ex00/test.py
+++ ex00/test.py
@@ -1,22 +1,5 @@
 import numpy as np
-
-
-def sigmoid_(x):
-    """
-    Compute the sigmoid of a vector.
-    Args:
-        x: has to be a numpy.ndarray of shape (m, 1).
-    Returns:
-        The sigmoid value as a numpy.ndarray of shape (m, 1).
-        None if x is an empty numpy.ndarray.
-    Raises:
-        This function should not raise any Exception.
-    """
-    if x.size == 0:
-        return None
-
-    return 1 / (1 + np.exp(-x))
-
+from ex00.sigmoid import sigmoid_
 
 np.set_printoptions(precision=17)
 
@@ -29,12 +12,12 @@ print("Expected: [[0.01798620996209156]]")
 
 # Example 2:
 x2 = np.array([[2]]).reshape((-1, 1))
-print("- Sigmoid Example 2:")
+print("\n- Sigmoid Example 2:")
 print("Calculated:", sigmoid_(x2))
 print("Expected: [[0.8807970779778823]]")
 
 # Example 3:
 x3 = np.array([[-4], [2], [0]]).reshape((-1, 1))
-print("- Sigmoid Example 3:")
+print("\n- Sigmoid Example 3:")
 print("Calculated:", sigmoid_(x3))
 print("Expected: [[0.01798620996209156], [0.8807970779778823], [0.5]]")
--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# make results directory
 if [ ! -d "results" ]; then
     mkdir results
 fi
 
+# make results/ex directory
 for i in {0..9}
 do
     if [ ! -d "results/ex0$i" ]; then
@@ -11,6 +13,13 @@ do
     fi
 done
 
+# get current directory
+currentDir=$(pwd)
+
+# Export the new path to PYTHONPATH
+export PYTHONPATH="$currentDir:$PYTHONPATH"
+
+# execute test.py
 for i in {0..9}
 do
     python3 ex0$i/test.py > results/ex0$i/result.txt
