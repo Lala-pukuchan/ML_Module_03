5c028b083ff25e7b9e15ed7a7bd60a1da21f31c5
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-27 10:51:46 +0100

    ex08, ex09

--- ex08/test.py
+++ ex08/test.py
@@ -8,19 +8,19 @@ y_hat = np.array([1, 1, 0, 1, 0, 0, 1, 1]).reshape((-1, 1))
 y = np.array([1, 0, 0, 1, 0, 1, 0, 0]).reshape((-1, 1))
 
 # Accuracy
-print("Accuracy - Your Implementation:", accuracy_score_(y, y_hat))
+print("Accuracy - My implementation:", accuracy_score_(y, y_hat))
 print("Accuracy - sklearn Implementation:", accuracy_score(y, y_hat))
 
 # Precision
-print("Precision - Your Implementation:", precision_score_(y, y_hat))
+print("Precision - My implementation:", precision_score_(y, y_hat))
 print("Precision - sklearn Implementation:", precision_score(y, y_hat))
 
 # Recall
-print("Recall - Your Implementation:", recall_score_(y, y_hat))
+print("Recall - My implementation:", recall_score_(y, y_hat))
 print("Recall - sklearn Implementation:", recall_score(y, y_hat))
 
 # F1-score
-print("F1 Score - Your Implementation:", f1_score_(y, y_hat))
+print("F1 Score - My implementation:", f1_score_(y, y_hat))
 print("F1 Score - sklearn Implementation:", f1_score(y, y_hat))
 
 # Example 2:
@@ -32,19 +32,19 @@ y = np.array(
 )
 
 # Accuracy
-print("Accuracy - Your Implementation:", accuracy_score_(y, y_hat))
+print("Accuracy - My implementation:", accuracy_score_(y, y_hat))
 print("Accuracy - sklearn Implementation:", accuracy_score(y, y_hat))
 
 # Precision
-print("Precision - Your Implementation:", precision_score_(y, y_hat, pos_label="dog"))
+print("Precision - My implementation:", precision_score_(y, y_hat, pos_label="dog"))
 print("Precision - sklearn Implementation:", precision_score(y, y_hat, pos_label="dog"))
 
 # Recall
-print("Recall - Your Implementation:", recall_score_(y, y_hat, pos_label="dog"))
+print("Recall - My implementation:", recall_score_(y, y_hat, pos_label="dog"))
 print("Recall - sklearn Implementation:", recall_score(y, y_hat, pos_label="dog"))
 
 # F1-score
-print("F1 Score - Your Implementation:", f1_score_(y, y_hat, pos_label="dog"))
+print("F1 Score - My implementation:", f1_score_(y, y_hat, pos_label="dog"))
 print("F1 Score - sklearn Implementation:", f1_score(y, y_hat, pos_label="dog"))
 
 # Example 3:
@@ -57,7 +57,7 @@ y = np.array(
 
 # Precision for 'norminet'
 print(
-    "Precision ('norminet') - Your Implementation:",
+    "Precision ('norminet') - My implementation:",
     precision_score_(y, y_hat, pos_label="norminet"),
 )
 print(
@@ -67,7 +67,7 @@ print(
 
 # Recall for 'norminet'
 print(
-    "Recall ('norminet') - Your Implementation:",
+    "Recall ('norminet') - My implementation:",
     recall_score_(y, y_hat, pos_label="norminet"),
 )
 print(
@@ -77,7 +77,7 @@ print(
 
 # F1-score for 'norminet'
 print(
-    "F1 Score ('norminet') - Your Implementation:",
+    "F1 Score ('norminet') - My implementation:",
     f1_score_(y, y_hat, pos_label="norminet"),
 )
 print(
--- ex09/test.py
+++ ex09/test.py
@@ -8,18 +8,18 @@ y = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet']).reshape(
 
 # Example 1:
 print("Example 1:")
-print("Your implementation:\n", confusion_matrix_(y, y_hat))
+print("My implementation:\n", confusion_matrix_(y, y_hat))
 print("sklearn implementation:\n", confusion_matrix(y, y_hat))
 
 # Example 2:
 print("\nExample 2:")
-print("Your implementation:\n", confusion_matrix_(y, y_hat, labels=['dog', 'norminet']))
+print("My implementation:\n", confusion_matrix_(y, y_hat, labels=['dog', 'norminet']))
 print("sklearn implementation:\n", confusion_matrix(y, y_hat, labels=['dog', 'norminet']))
 
 # Example 3:
 print("\nExample 3:")
-print("Your implementation (DataFrame):\n", confusion_matrix_(y, y_hat, df_option=True))
+print("My implementation (DataFrame):\n", confusion_matrix_(y, y_hat, df_option=True))
 
 # Example 4:
 print("\nExample 4:")
-print("Your implementation (DataFrame with specified labels):\n", confusion_matrix_(y, y_hat, labels=['bird', 'dog'], df_option=True))
+print("My implementation (DataFrame with specified labels):\n", confusion_matrix_(y, y_hat, labels=['bird', 'dog'], df_option=True))
